c7ae2cd311c5463e4f3d387d6bf0e3e86d09637f
jeonga127 <71822288+jeonga127@users.noreply.github.com>
2023-04-13 18:32:19 +0900

    [level 2] Title: 모음 사전, Time: 8.46 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
@@ -6,25 +6,27 @@ class Solution {
     // a e i o u 로 만들 수 있는 경우의 수를 다 탐색 
     public int solution(String word) {
         int answer = 1;
-        dfs(dict,"");
-    
+        dfs(dict,"", word);
+        
         for(String d : dict){
             if(d.equals(word))
                 break;
             else
                 answer++;
         }
-    
+        
         return answer;
     } 
     
-    static void dfs(List<String> dict, String current){
+    static void dfs(List<String> dict, String current, String target){
+        if(current.equals(target))
+            return;
         if(current.length() == 5)
             return;
         
         for(int i = 0; i < 5; i++){
             dict.add(current + aeiou[i]);
-            dfs(dict, current + aeiou[i]);
+            dfs(dict, current + aeiou[i], target);
         }
         
     }
