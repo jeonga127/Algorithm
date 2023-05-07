f58f67537a616bb5cb026fb9d29554a81afc4c49
jeonga127 <71822288+jeonga127@users.noreply.github.com>
2023-05-07 09:48:21 +0900

    [Bronze V] Title: 아스키 코드, Time: 220 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        char input = sc.nextLine().charAt(0);
+        System.out.print((int) input);
+    }
+}
