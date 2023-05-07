10f0b7227f55cf190fac093e1f256c15b09f7097
jeonga127 <71822288+jeonga127@users.noreply.github.com>
2023-05-07 13:02:32 +0900

    [Bronze V] Title: 윤년, Time: 124 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int year = Integer.parseInt(br.readLine());
+        System.out.print(((year % 4 == 0 && year % 100 != 0) || year % 400 ==0)? "1" : "0");
+    }
+}
