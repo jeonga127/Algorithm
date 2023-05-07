b87a247e5f2cf438c1b25ee989a47a387220154e
jeonga127 <71822288+jeonga127@users.noreply.github.com>
2023-05-07 22:33:58 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int L = Integer.parseInt(br.readLine());
+        String[] input = br.readLine().split("");
+        int[] arr = new int[L];
+
+        for (int i = 0; i < L; i++)
+            arr[i] = input[i].charAt(0) - 'a' + 1;
+
+        int H = 0;
+        for (int i = 0; i < L; i++) {
+            H += arr[i] * Math.pow(31, i);
+        }
+        System.out.print(H % 1234567891);
+    }
+}
