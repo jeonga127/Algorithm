4c9acf22c94d9af59e6ec360690cf9b09a891829
jeonga127 <71822288+jeonga127@users.noreply.github.com>
2023-05-08 01:17:54 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1440 ms, Memory: 57532 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,31 @@
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        List<Point> answer = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            answer.add(new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        }
+
+        Collections.sort(answer, new Comparator<Point>() {
+            @Override
+            public int compare(Point o1, Point o2) {
+                if (o1.x > o2.x) return 1;
+                else if (o1.x < o2.x) return -1;
+                else if (o1.y > o2.y) return 1;
+                else if (o1.y < o2.y) return -1;
+                else return 0;
+            }
+        });
+        answer.forEach(x -> System.out.println(x.x + " " + x.y));
+    }
+}
