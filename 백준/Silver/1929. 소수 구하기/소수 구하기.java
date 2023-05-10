3ca8d4fb30c71638579c160bd4a8331f7871e78f
jeonga127 <71822288+jeonga127@users.noreply.github.com>
2023-05-10 09:08:34 +0900

    [Silver III] Title: 소수 구하기, Time: 464 ms, Memory: 20296 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.stream.IntStream;
+
+public class Main {
+    static boolean isDecimal(int num) {
+        if (num == 1) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        IntStream.range(M, N + 1).filter(Main::isDecimal).forEach(x -> sb.append(x).append("\n"));
+        
+        System.out.print(sb);
+    }
+}
