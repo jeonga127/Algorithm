3f97db70d181ec89a8954586626a9e9586785431
jeonga127 <71822288+jeonga127@users.noreply.github.com>
2023-05-14 22:01:00 +0900

    [Silver IV] Title: 2+1 세일, Time: 360 ms, Memory: 27104 KB -BaekjoonHub

---
+++ 백준/Silver/11508. 2＋1 세일/2＋1 세일.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] price = new int[N];
+
+        for (int i = 0; i < N; i++)
+            price[i] = Integer.parseInt(br.readLine());
+        Arrays.sort(price);
+
+        int idx = 1;
+        int answer = 0;
+        for (int i = N - 1; i >= 0; i--) {
+            if (idx % 3 != 0)
+                answer += price[i];
+            idx++;
+        }
+        System.out.print(answer);
+    }
+}
