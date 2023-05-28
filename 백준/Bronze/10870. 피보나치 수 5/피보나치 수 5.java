279d3976856ff50c046f5f021f95920faa17b592
jeonga127 <71822288+jeonga127@users.noreply.github.com>
2023-05-29 03:23:51 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 120 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        if (N < 2) {
+            System.out.print(N);
+            return;
+        }
+        
+        int[] arr = new int[N + 1];
+        arr[0] = 0;
+        arr[1] = 1;
+
+        for (int i = 2; i < N + 1; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+
+        System.out.print(arr[N]);
+    }
+}
