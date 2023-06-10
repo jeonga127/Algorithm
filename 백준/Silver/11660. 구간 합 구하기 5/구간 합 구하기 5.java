9ec470b3760e77ed79acb68cae138eb3b4274b4c
jeonga127 <71822288+jeonga127@users.noreply.github.com>
2023-06-10 22:25:09 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 896 ms, Memory: 120956 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -12,7 +12,7 @@ public class Main {
         int N = Integer.parseInt(st.nextToken()); // 표의 크기
         int M = Integer.parseInt(st.nextToken()); // 합을 구하는 횟수
         int[][] table = new int[N][N];
-        int[][] sum = new int[N][N];
+        int[][] sum = new int[N + 1][N + 1];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -20,34 +20,27 @@ public class Main {
                 table[i][j] = Integer.parseInt(st.nextToken());
         }
 
-        sum[0][0] = table[0][0];
+        sum[1][1] = table[0][0];
 
-        for (int i = 1; i < N; i++) {
-            sum[i][0] = sum[i - 1][0] + table[i][0];
-            sum[0][i] = sum[0][i - 1] + table[0][i];
+        for (int i = 2; i < N + 1; i++) {
+            sum[i][1] = sum[i - 1][1] + table[i - 1][0];
+            sum[1][i] = sum[1][i - 1] + table[0][i - 1];
         }
 
-        for (int i = 1; i < N; i++) {
-            for (int j = 1; j < N; j++)
-                sum[i][j] = table[i][j] + sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1];
+        for (int i = 2; i < N + 1; i++) {
+            for (int j = 2; j < N + 1; j++)
+                sum[i][j] = table[i - 1][j - 1] + sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1];
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
 
-            int x1 = Integer.parseInt(st.nextToken()) - 1;
-            int y1 = Integer.parseInt(st.nextToken()) - 1;
-            int x2 = Integer.parseInt(st.nextToken()) - 1;
-            int y2 = Integer.parseInt(st.nextToken()) - 1;
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
 
-            if(x1 == 0 && y1 == 0)
-                sb.append(sum[x2][y2]).append("\n");
-            else if(x1 == 0)
-                sb.append(sum[x2][y2] - sum[x2][y1 - 1]).append("\n");
-            else if(y1 == 0)
-                sb.append(sum[x2][y2] - sum[x1 - 1][y2]).append("\n");
-            else
-                sb.append(sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][y1 - 1] + sum[x1 - 1][y1 - 1]).append("\n");
+            sb.append(sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][y1 - 1] + sum[x1 - 1][y1 - 1]).append("\n");
         }
 
         System.out.print(sb);
