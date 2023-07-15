86b4a3265f48819b010268300b014b38a15f1ec1
jeonga127 <71822288+jeonga127@users.noreply.github.com>
2023-07-15 13:57:43 +0900

    [Gold IV] Title: 육각수, Time: 1512 ms, Memory: 50592 KB -BaekjoonHub

--- 백준/Gold/1229. 육각수/육각수.java
+++ 백준/Gold/1229. 육각수/육각수.java
@@ -5,42 +5,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    static List<Integer> hexNumList = new ArrayList<>();
-    static int[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<Integer> hexNumList = new ArrayList<>();
+        int N = Integer.parseInt(br.readLine());
 
-    public static void getHexNumber(int N) {
+        int[] dp = new int[N + 1];
+        for (int i = 1; i < Math.min(N + 1, 6); i++)
+            dp[i] = i;
+
         int idx = 1;
         int nearHexNum;
 
         while (true) {
             nearHexNum = idx * (2 * idx - 1);
 
-            if (nearHexNum > N) return;
+            if (nearHexNum > N) break;
             else {
                 hexNumList.add(nearHexNum);
                 dp[nearHexNum] = 1;
-
-                if (2 * nearHexNum <= N)
-                    dp[2 * nearHexNum] = dp[2 * nearHexNum] > 0 ? Math.min(dp[2 * nearHexNum], 2) : 2;
             }
             idx += 1;
         }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-
-        dp = new int[N + 1];
-        for (int i = 1; i < Math.min(N + 1, 6); i++)
-            dp[i] = i;
-
-        if (N > 11) dp[11] = 6;
-        if (N > 26) dp[26] = 6;
-        if (N > 130) dp[130] = 5;
-        if (N > 146858) dp[146858] = 4;
-
-        getHexNumber(N);
 
         for (int i = 6; i < N + 1; i++) {
             if (dp[i] > 0)
@@ -59,4 +45,5 @@ public class Main {
 
         System.out.print(dp[N]);
     }
+
 }
