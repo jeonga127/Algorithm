378e821cd99485df91b134300304827935d3a2ee
jeonga127 <71822288+jeonga127@users.noreply.github.com>
2023-07-15 13:13:18 +0900

    [Gold IV] Title: 육각수, Time: 1528 ms, Memory: 50232 KB -BaekjoonHub

--- 백준/Gold/1229. 육각수/육각수.java
+++ 백준/Gold/1229. 육각수/육각수.java
@@ -19,6 +19,9 @@ public class Main {
             else {
                 hexNumList.add(nearHexNum);
                 dp[nearHexNum] = 1;
+
+                if (2 * nearHexNum <= N)
+                    dp[2 * nearHexNum] = dp[2 * nearHexNum] > 0 ? Math.min(dp[2 * nearHexNum], 2) : 2;
             }
             idx += 1;
         }
@@ -32,21 +35,21 @@ public class Main {
         for (int i = 1; i < Math.min(N + 1, 6); i++)
             dp[i] = i;
 
-        if(N > 11) dp[11] = 6;
-        if(N > 26) dp[26] = 6;
-        if(N > 130) dp[130] = 5;
-        if(N > 146858) dp[146858] = 4;
+        if (N > 11) dp[11] = 6;
+        if (N > 26) dp[26] = 6;
+        if (N > 130) dp[130] = 5;
+        if (N > 146858) dp[146858] = 4;
 
         getHexNumber(N);
 
-        for(int i = 6; i < N + 1; i++){
-            if(dp[i] > 0)
+        for (int i = 6; i < N + 1; i++) {
+            if (dp[i] > 0)
                 continue;
 
             int min = Integer.MAX_VALUE;
 
-            for(int hexNum : hexNumList){
-                if(hexNum > i || min == 1)
+            for (int hexNum : hexNumList) {
+                if (hexNum > i || min == 1)
                     break;
                 min = Math.min(min, dp[i - hexNum]);
             }
