354fd5858134c4fd21cc9457a4b0362f13bbcffe
jeonga127 <71822288+jeonga127@users.noreply.github.com>
2023-07-17 10:52:09 +0900

    [Gold V] Title: 평범한 배낭, Time: 164 ms, Memory: 15608 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
